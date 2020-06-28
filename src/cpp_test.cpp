14c26dd927d789d9211db53b0c00751bbdc1052c
eterpega <eterpega@gmail.com>
2020-06-28 23:49:21 +0900

    Update cpp_test.cpp

--- src/cpp_test.cpp
+++ src/cpp_test.cpp
@@ -1,9 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "../include/cpp_test.h"
 
 TestClass TC;
 int main(int argc, char* argv[])
 {
-  int ff;
+  int ff=10;
+  printf("out: %d\n", ff);
   int rtn = TC.fn1(ff, 30);
   printf("out: %d\n", ff);
   return rtn;
